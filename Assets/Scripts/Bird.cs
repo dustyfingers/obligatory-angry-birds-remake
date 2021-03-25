c9812e39423b9477e3f516fcaf30754a1e3593e6
lou <dustyfingers01257@gmail.com>
2021-03-25 12:53:01 -0600

    initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float launchForce = 500f;
+
+    Vector2 startPosition;
+    Rigidbody2D rigidbody;
+    SpriteRenderer spriteRenderer;
+
+    void Awake ()
+    {
+        rigidbody = GetComponent<Rigidbody2D> ();
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        startPosition = rigidbody.position;
+        rigidbody.isKinematic = true;
+    }
+
+    void OnMouseDown ()
+    {
+        spriteRenderer.color = Color.red;
+    }
+
+    void OnMouseUp ()
+    {
+        Vector2 currentPosition = rigidbody.position;
+        Vector2 direction = startPosition - currentPosition;
+        direction.Normalize();
+
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce (direction * launchForce);
+        spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag ()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        transform.position = new Vector3 (mousePos.x, mousePos.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        StartCoroutine (ResetAfterDelay ());
+    }
+
+    IEnumerator ResetAfterDelay ()
+    {
+        yield return new WaitForSeconds (2f);
+        rigidbody.position = startPosition;
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.isKinematic = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+step 1: create an art folder in the assets folder
+step 2: unzip the art from here into it: https://drive.google.com/drive/folders/19F5GBDXAfyNK9x_P_yct_aBim68f7L69?usp=sharing
+step 3: ???
+step 4: definitely do not profit because of copyright
